caf01ebe3ad16a38d8ec5272accaa519a289e83a
Valentyn Fediuk <valentynfediuk@gmail.com>
2025-03-31 01:00:46 +0300

    hero

--- src/js/hero-slider.js
+++ src/js/hero-slider.js
@@ -3,8 +3,8 @@ import 'slick-carousel';
 
 document.addEventListener('DOMContentLoaded', () => {
   $('.hero__slider').slick({
-    // autoplay: true,
-    // autoplaySpeed: 3000,
+    autoplay: true,
+    autoplaySpeed: 3000,
     dots: true,
     arrows: false,
     infinite: true,
